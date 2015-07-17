356a00240f7ba8b5068fa5226ed6b1b0efafbd79
noramaxwell <nora_maxwell@dalton.org>
2015-07-17 16:28:01 +0000

    final

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+ 
+$("h1").click(function() {
+  window.location.href='index.html';
+});  
+  
+ $("#hbk").click(function() {
+ $("<p>hi</p>").toggle(1000);
+});  
+ 
+
+}); //end
